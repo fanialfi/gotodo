3fb0909f90a7554ed1a6943d3ccf07fa5ce528e1
fanialfi <fanialfirdaus@gmail.com>
2024-12-26 10:44:32 +0700

    add flag for add task command

--- cmd/add.go
+++ cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"errors"
+	"log"
 
 	"github.com/fanialfi/gotodo/internal/task"
 	"github.com/spf13/cobra"
@@ -12,16 +13,28 @@ func NewAddCMD() *cobra.Command {
 		Use:   "add",
 		Short: "add task to the task list",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runAddTaskCMD(args)
+			return runAddTaskCMD(cmd)
 		},
+	}
+
+	cmd.Flags().String("description", "", "name task")
+	err := cmd.MarkFlagRequired("description")
+	if err != nil {
+		log.Printf("Error marking flags as required : %s\n", err.Error())
+		return nil
 	}
 	return cmd
 }
 
-func runAddTaskCMD(args []string) error {
-	if len(args) == 0 {
-		return errors.New("task description is required")
+func runAddTaskCMD(cmd *cobra.Command) error {
+	desctiption, err := cmd.Flags().GetString("description")
+	if err != nil {
+		return err
 	}
-	desctiption := args[0]
+
+	if len(desctiption) == 0 {
+		return errors.New("description for task is required")
+	}
+
 	return task.AddTask(desctiption)
 }
